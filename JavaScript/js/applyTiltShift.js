e647efcfb39752652c3292a4f2acd5dfc263322b
Poligraph <nacar@tut.by>
2012-10-05 20:02:30 +0300

    Use tiltShift plugin

---
+++ JavaScript/js/applyTiltShift.js
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Evgeniy
+ * Date: 05.10.12
+ * Time: 19:44
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+    $(".tiltshift").tiltShift();
+})
