b77789d11ad8d645d2c9c86521552ad7bc473356
Poligraph <nacar@tut.by>
2012-10-21 12:26:25 +0300

    Add .gitignore

--- HTML/js/oop.js
+++ HTML/js/oop.js
@@ -7,76 +7,23 @@
  * To change this template use File | Settings | File Templates.
  */
 $(document).ready(function(){
-    function Mammals(weight){
-        this.weight=weight;
-    }
-    var mammals=new Mammals(60);
-function Animal(name,age){
-    this.name=name;
-    this.age=age;
-}
-    Animal.prototype=mammals;
-var anim=new Animal("Kitty",3);
-function Cat(name){
-    this.name=name;
-}
-Cat.prototype=anim;
-var cat=new Cat("Gik");
-//console.log("name:"+cat.name);
-//console.log("age:"+cat.age);
-//console.log("weight:"+cat.weight);
-    var michail=Object.create(null);
-    Object.defineProperty(michail,"last_name",{value:"Michail",
-                                            writable:true,
-                                            configurable:true,
-                                            enumerable:true});
-    Object.defineProperty(michail,"first_name",{value:"Baburovich",
-                                            writable:true,
-                                            configurable:true,
-                                            enumerable:true});
-    Object.defineProperty(michail,"age",{value:19,
-                                         writable:true,
-                                         configurable:true,
-                                         enumerable:true});
-    Object.defineProperty(michail,"gender",{value:"Male",
-                                         writable:true,
-                                         configurable:true,
-                                         enumerable:true});
-    function get_full_name(){
-        return this.first_name+" "+this.last_name;
-    }
-    function set_full_name(new_name){
-        var names=new_name.trim().split(/\s+/)
-            this.first_name=names["0"]||"";
-        this.last_name=names["1"]||"";
-    }
-    Object.defineProperty(michail,"name",{get:get_full_name,set:set_full_name,configurable:true,enumerable:true});
-    console.log("FirstName: "+michail["first_name"]);
-    console.log("LastName: "+michail["last_name"]);
-    console.log("FullName:"+michail.name);
-//    delete michail["age"];
-    console.log("Age: "+michail["age"]);
-//    delete michail["gender"];
-    console.log("Gender: "+michail["gender"]);
-    console.log("AllPropertyGetOwnPropertyNames: "+Object.getOwnPropertyNames(michail));
-    console.log("AllPropertyKeys: "+Object.keys(michail));
-    var andrey={
-        first_name:"Grin",
-        last_name:"Andrey",
-        human_age:19,
-        gender:"Male",
-        get name(){
-            return this.first_name+" "+this.last_name;
-        },
-        set name(new_name){
-            var names=new_name.trim().split(/\s+/);
-            this.first_name=names["0"] || ""
-            this.last_name=names["1"] || ""
-        },
-        get age(){
-            return this.human_age+" years";
-        }
-    }
-        console.log("FullName: "+andrey.name);
-        console.log("Age: "+andrey.age);
+    var human=Object.create(null);
+    Object.defineProperty(human,"name",{value:"Poligraph",writable:true,configurable:true,enumerable:false});
+    Object.defineProperty(human,"firstName",{value:"Sharicoff",writable:true,configurable:true,enumerable:false});
+    Object.defineProperty(human,"fullName",{get:function(){return this.name+' '+this.firstName},configurable:true,enumerable:true});
+    Object.defineProperty(human,"age",{value:18,writable:false,configurable:false,enumerable:false});
+//    Аналогично предыдущему
+//    Object.defineProperties(human,{name:{value:"Evgenii",writable:true,configurable:true,enumerate:true}
+//    ,age:{value:18,writable:true,configurable:true,enumerate:true}});
+
+    console.log("Name:"+human["name"]);
+    console.log("Age:"+human["age"]);
+    delete human.age;//Поле age нельзя удалить, параметр configurable=false
+    human.age=12;//Поле age является неизменяемым, параметр writable=false
+    console.log("Age:"+human["age"]);
+    console.log("FullName: "+human["fullName"]);
+    console.log("AllFields: "+Object.getOwnPropertyNames(human));
+    console.log("Поля с флагом enumerable: "+Object.keys(human));
+
+
 });
