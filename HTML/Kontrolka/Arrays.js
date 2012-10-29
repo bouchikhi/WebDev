172255f9fa64f422173436ee6c2a6bf05887ea1b
Poligraph <nacar@tut.by>
2012-10-29 10:34:41 +0300

    Added Twitter Bootstrap

---
+++ HTML/Kontrolka/Arrays.js
@@ -0,0 +1,7 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Evgeniy
+ * Date: 25.10.12
+ * Time: 8:50
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ JavaScript/js/AutoScrollPage.js
@@ -0,0 +1,7 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Evgeniy
+ * Date: 21.10.12
+ * Time: 16:39
+ * To change this template use File | Settings | File Templates.
+ */
