c3cb23d1819547d8d87f4fd99934e4fe6028254d
Poligraph <nacar@tut.by>
2012-10-29 10:50:51 +0300

    Add Twitter Bootstrap

--- HTML/Kontrolka/Arrays.js
+++ HTML/Kontrolka/Arrays.js
@@ -5,3 +5,14 @@
  * Time: 8:50
  * To change this template use File | Settings | File Templates.
  */
+function searchItem(array){
+    for(var i=0;i<array.length;i++){
+        if(array[i]==0){
+            return i;
+        }
+    }
+}
+function testArraySearch(){
+    var mas=[1,2,0,4,5,6,7,0];
+    document.writeln("Исходный массив:"+mas+"<br>"+"Позиция первого элемента равного 0: "+searchItem(mas));
+}
--- JavaScript/js/AutoScrollPage.js
+++ JavaScript/js/AutoScrollPage.js
@@ -5,3 +5,9 @@
  * Time: 16:39
  * To change this template use File | Settings | File Templates.
  */
+var smoothJumpUp = function() {
+    if (document.body.scrollTop>0 || document.documentElement.scrollTop>0) {
+        window.scrollBy(0,-250);
+        setTimeout(smoothJumpUp, 1);
+    }
+}
